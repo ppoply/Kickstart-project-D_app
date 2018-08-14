9b4a03efbdd9a3b4ddccb622f335e7d036f62761
ppoply <34216657+ppoply@users.noreply.github.com>
2018-08-14 23:58:48 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 
 class CampaignIndex extends Component {
@@ -15,7 +16,11 @@ class CampaignIndex extends Component {
 		const items = this.props.campaigns.map(address => {
 			return {
 				header: address,
-				description: <a>View Campaign</a>,
+				description: (
+					<Link route={`/campaigns/${address}`}>
+						<a>View Campaign</a>
+					</Link>
+				),
 				fluid: true
 			};
 		});
@@ -27,13 +32,17 @@ class CampaignIndex extends Component {
 		return (
 			<Layout>
 				<div> 
-					<h3> Open Campaigns </h3>				
-					<Button
-						content="Create Campaign"
-						icon="add"
-						floated="right"
-						primary
-					/>
+					<h3> Open Campaigns </h3>
+					<Link route="/campaigns/new">
+						<a>				
+							<Button
+								content="Create Campaign"
+								icon="add"
+								floated="right"
+								primary
+							/>
+						</a>
+					</Link>
 					{ this.renderCampaigns() }
 				</div>
 			</Layout>
---
+++ routes.js
@@ -0,0 +1,5 @@
+// File for setting up dynamic routes using next-route library
+
+const routes = require('next-routes')();
+
+module.exports = routes;
