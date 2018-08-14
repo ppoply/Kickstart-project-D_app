c1c092f05aa85d088d3f678d476eb338f6e9ec44
ppoply <34216657+ppoply@users.noreply.github.com>
2018-08-14 17:01:15 +0530

    Add files via upload

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,18 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+	// We are in the browser and metamask is running. 
+	web3 = new Web3(window.web3.currentProvider);
+}
+else {
+	// We are on the server *OR* the user is not running metamask
+	const provider = new Web3.providers.HttpProvider(
+		'https://rinkeby.infura.io/c37a4573e4874debb6aeca6f570638df'
+	); // Setting up our own provider through infura
+	
+	web3 = new Web3(provider);
+}
 
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
+import { Card, Button } from 'semantic-ui-react';
+import Layout from '../components/Layout';
 
-export default () => {
-	return (
-		<h1> This is the campaign list page !!! </h1>
-	);
-};
+
+class CampaignIndex extends Component {
+
+	static async getInitialProps() {
+		const campaigns = await factory.methods.getDeployedCampaigns().call();
+		return { campaigns };
+	}
+
+	renderCampaigns() {
+		const items = this.props.campaigns.map(address => {
+			return {
+				header: address,
+				description: <a>View Campaign</a>,
+				fluid: true
+			};
+		});
+
+		return <Card.Group items = {items} />;
+	}
+
+	render() {
+		return (
+			<Layout>
+				<div> 
+					<h3> Open Campaigns </h3>				
+					<Button
+						content="Create Campaign"
+						icon="add"
+						floated="right"
+						primary
+					/>
+					{ this.renderCampaigns() }
+				</div>
+			</Layout>
+		);
+	}
+}
+
+export default CampaignIndex; 
